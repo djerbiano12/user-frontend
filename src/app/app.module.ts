f35dca76dcd357da3ad83512b78ac2d76cca05ca
Amine <aminebms4@gmail.com>
2018-11-03 13:06:11 +0100

    ajout du composant dauthentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +20,9 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule
   ],
   providers: [UserService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
 })
 export class AppModule { }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserCreateComponent } from './user-create/user-create.component';
-import { UserService } from './user.service'
+import { UserService } from './user.service';
+import { LoginComponent } from './login/login.component'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],providers:[
   	UserService
   ],
-  declarations: [UserListComponent, UserCreateComponent]
+  declarations: [UserListComponent, UserCreateComponent, LoginComponent]
 })
 export class UserModule { }
