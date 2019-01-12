4f84605d5a322c2c3c259df6ad0e2461954cce76
Amine <aminebms4@gmail.com>
2019-01-12 14:47:17 +0100

    ajout UPDATE/CREATE User

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,27 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from "../User";
 import { UserService } from "../user.service";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
-  providers: [ UserService ]
+  providers: [UserService]
 })
 export class UserListComponent implements OnInit {
 
   private users: User[];
 
-  constructor(private userService: UserService) { }
+  constructor(private router: Router,
+              private userService: UserService) { }
 
-  ngOnInit() { //when component loading get all users and set the users[]
+  ngOnInit() {
     this.getAllUsers();
   }
 
   getAllUsers() {
-    this.userService.getUsers()
-      .subscribe( data => {
-        this.users = data;
-      });
+    this.userService.getUsers().subscribe(
+      users => {
+        this.users = users;
+      },
+      err => {
+        console.log(err);
+      }
+
+    );
   }
+
+  redirectNewUserPage() {
+    this.router.navigate(['/user/create']);
+  }
+
+  editUserPage(user: User) {
+    if (user) {
+      this.router.navigate(['/user/edit', user.id]);
+    }
+  }
+
+  deleteUser(user: User) {
+    console.log('Delete User');
+  }
+
 }
